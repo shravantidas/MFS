d3f33f058cbe36a2c20f332839a05a54c52aaadc
shravantidas <shravantidastithi@gmail.com>
2024-10-02 10:48:01 +0600

    getelement function added here

---
+++ js/home.js
@@ -0,0 +1,5 @@
+document.getElementById('add-money').addEventListener('click',function(event){
+event.preventDefault();
+const addmoney=document.getElementById('amount').value;
+const pinNumber=document.getElementById('pin').value;
+})

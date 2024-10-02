26bae21d3c0ffded729e35bd6eb77f069a18bd17
shravantidas <shravantidastithi@gmail.com>
2024-10-02 11:24:10 +0600

    balance updated

--- js/home.js
+++ js/home.js
@@ -2,4 +2,13 @@ document.getElementById('add-money').addEventListener('click',function(event){
 event.preventDefault();
 const addmoney=document.getElementById('amount').value;
 const pinNumber=document.getElementById('pin').value;
+if(pinNumber=='1234'){
+    const balance=document.getElementById('balance').innerText;
+    const money=parseFloat(addmoney);
+    const balanceNumber=parseFloat(balance);
+    const newbalance=balanceNumber+money;
+    console.log(newbalance);
+    document.getElementById('balance').innerText=newbalance;
+
+}
 })
